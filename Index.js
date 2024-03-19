0d32b2276e451b7eedc8f6ac2996654d38576a46
MaximeR37 <163993158+MaximeR37@users.noreply.github.com>
2024-03-19 16:53:20 +0100

    Modif

--- Index.js
+++ Index.js
@@ -1,4 +1,6 @@
 
+var PageLoc = "./Pages"
+
 var SceneData = $("a-scene")
 var scene     = SceneData[0]
 var MainScene = $("#MainScene")[0]
@@ -24,5 +26,31 @@ async function SwitchArea(Name) {
     $("#cur_camera")[0].emit("start_trans")
     await sleep(500)
   
-    MainScene.attributes.template.nodeValue = "src: " + "./resources/pages/" + PathName + "/" + Name + ".html"
+    MainScene.attributes.template.nodeValue = "src: " + PageLoc + "/" + PathName + "/" + Name + ".html"
 }
+
+AFRAME.registerComponent('scene-init', {
+    schema: {type: 'string', default: 'default'},
+    init: async function() {
+      console.log("init")
+      this.SceneName = this.data
+      console.log(this.SceneName)
+  
+      SwitchArea(this.SceneName)
+    }
+  })  
+
+AFRAME.registerComponent('scene-changer', {
+    schema: {type: 'int', default: 0},
+  
+    init: async function() {
+      this.onClick = this.onClick.bind(this)
+      this.SceneName = this.data["name"]
+
+      this.el.addEventListener("click", this.onClick)
+    },
+  
+    onClick: async function() {
+      SwitchArea(this.SceneName)
+    }
+  })
