977a28a54e6aeffcd9656677502def330fc0c80d
MaximeR37 <163993158+MaximeR37@users.noreply.github.com>
2024-03-20 09:39:51 +0100

    Réorganisation

---
+++ partie3/index.js
@@ -0,0 +1,64 @@
+// Variable qui contien la localisation des pages
+var PageLoc = "./Pages"
+
+// Variables pour A-FRAME corsernant la scene
+var SceneData = $("a-scene")
+var scene     = SceneData[0]
+var MainScene = $("#MainScene")[0]
+
+// Contien le nom du document dans une variable
+let PathName = location.pathname.split("/")
+PathName = (PathName[PathName.length - 1].split(".")[0] || "index").toUpperCase()
+
+// Function pour attendre x millisecondes
+function sleep(ms) { return new Promise(resolve => setTimeout(resolve, ms)); }
+
+// Function qui enlève le "cache" de la caméra
+async function UpdateNavigator() {
+    await sleep(100)
+    $("#cur_camera")[0].emit("end_trans")
+  }
+
+// Interaction pour la fonction UpdateNavigator, quand la template est charger
+if(MainScene) MainScene.addEventListener("templaterendered", UpdateNavigator)
+
+// Function qui change la scene en vue du nom donner
+async function SwitchArea(Name) {
+    let ok = document.querySelectorAll(".field")
+
+    // Enlève tous les élement de la classe "field" 
+    ok.forEach(function(val) { $(val).remove() })
+
+    $("#cur_camera")[0].emit("start_trans")
+    await sleep(500)
+  
+    // Changement de la scene par la valeur du template
+    MainScene.attributes.template.nodeValue = "src: " + PageLoc + "/" + PathName + "/" + Name + ".html"
+}
+
+// Initialisation de la scene
+AFRAME.registerComponent('scene-init', {
+    schema: {type: 'string', default: 'default'},
+    init: async function() {
+      this.SceneName = this.data
+
+      SwitchArea(this.SceneName)
+    }
+  })  
+
+// Button qui change la scene a la valeur prédéfinie
+AFRAME.registerComponent('scene-changer', {
+    schema: {type: 'string', default: 'default'},
+  
+    init: async function() {
+      this.onClick = this.onClick.bind(this)
+      this.SceneName = this.data
+
+      // Active l'evenement si un click est détecter
+      this.el.addEventListener("click", this.onClick)
+    },
+  
+    onClick: async function() {
+      SwitchArea(this.SceneName)
+    }
+  })
